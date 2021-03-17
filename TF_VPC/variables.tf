ec139e27ee20d1258530fd5916e3129e8332bbe1
Jun MA <ma.jun2@husky.neu.edu>
2021-03-18 03:25:25 +0800

    test1

--- TF_VPC/variables.tf
+++ TF_VPC/variables.tf
@@ -1,21 +1,30 @@
 variable "access_key" {
+    default = "AKIA6QQQ6KYRU2ELPI5W"
     description = "aws_IAM_ROLE_access_key"
 }
-variable "secret_key" {    
+variable "secret_key" {
+    default = "oLcQteB3Tauw3HyV/OIRyNmA1grNPJk/va8rc20L"
     description = "aws_IAM_ROLE_secret_key"
 }
 variable "EC2_key_name" {
+    default = "csye7220"
     description = "key name of the EC2"
 }
 variable "aws_region" {
+    default = "us-west-2"
 }
 variable "availability_zone"{
+    default = "us-west-2a"
 }
 variable "AMI_ID"{
+    default = "ami-0ca5c3bd5a268e7db"
 }
 variable "mongoip"{
+    default = "mongodb+srv://CSYE7220_MIDTERM:CSYE7220_MIDTERM@cluster0.lxsro.mongodb.net/Uber?retryWrites=true&w=majority"
 }
+
 variable "private_key_location"{
+    default = "D:/7220/CSYE7220/Key/csye7220_oregon.pem"
 }
 
 variable "cidr_block" {
